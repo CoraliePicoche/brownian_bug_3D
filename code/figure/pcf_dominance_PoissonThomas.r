955058501ce2a24991d80cb83e99645d1fc62a56
Coralie Picoche <coralie.picoche@u-bordeaux.fr>
2021-10-20 18:31:39 +0200

    Figures to check Poisson and Thomas distribution

--- code/figure/pcf_dominance_PoissonThomas.r
+++ code/figure/pcf_dominance_PoissonThomas.r
@@ -17,7 +17,6 @@ log10Tck <- function(side, type){
   ))
 }
 
-
 #Theoretical value of G
 G_theoretical=function(gamma,r,lambda,Delta,C_0,Tmax=NA){ #U=0 in the absence of advection
   tau=1
@@ -36,13 +35,13 @@ G_theoretical=function(gamma,r,lambda,Delta,C_0,Tmax=NA){ #U=0 in the absence of
 
 #####################################
 
-colo=c("red","blue","green","orange","grey")
-volume=10
+colo=c("red","blue","grey","orange","violet")
 
-pdf("cross_pcf_2sp_BBM_test_pcf.pdf",width=16,height=12)
-par(mfrow=c(2,3))
+pdf("metrics_PoissonThomas.pdf",width=15,height=9)
+par(mfrow=c(2,4),cex=1.,mar=c(5,5,0.5,1))
 
-f=read.table("../simulation/pcf_BBM_kernel_2sp_realistic_values_20000_U0p5_test_pcf.txt",sep=";",header=F,dec=".")
+nb_simu=1 #Poisson
+f=read.table(paste("../simulation/pcf_",nb_simu,".txt",sep=""),sep=";",header=F,dec=".")
 colnames(f)=c("r","sp1","sp2","pcf","dominance")
 unique_sp=unique(f$sp1)
 if (length(unique_sp)>length(colo)){
@@ -50,15 +49,23 @@ if (length(unique_sp)>length(colo)){
 }
 
 #Count species
-f_count=read.table("../simulation/nb_indiv_BBM_kernel_2sp_realistic_values_20000_U0p5_test_pcf.txt",header=F,sep=";",dec='.')
+f_count=read.table(paste("../simulation/nb_indiv_",nb_simu,".txt",sep=""),header=F,sep=";",dec='.')
 colnames(f_count)=c("species","abundance")
 
-dd=c()
+#Param simu
+f_param=read.table(paste("../simulation/param_",nb_simu,".txt",sep=""),header=F,sep="=",dec='.')
+f_param[,2]=as.numeric(as.character(f_param[,2]))
 
+dd=c()
 for (s1 in 1:length(unique_sp)){
-	plot(0.1,0.1,t="n",log="xy",xlab="r",ylab="",axes=F,ylim=c(0.5,max(f$pcf,na.rm=T)),xlim=range(f$r)+10^(-4),cex.lab=1.5)
+	if(s1==1){
+		yl="g(r)"
+	}else{
+		yl=""
+	}	
+	plot(0.1,0.1,t="n",log="xy",xlab="",ylab=yl,axes=F,xlim=range(f$r[f$r>0]),cex.lab=1.5,ylim=range(f$pcf))
 	f_plot=subset(f,sp1==unique_sp[s1]&sp2==unique_sp[s1])
-  	lines(f_plot$r,G_theoretical(0.5,f_plot$r,0.0002,7*10^(-5),f_count$abundance[f_count$species==(s1-1)]/volume),col="black",lty=2,lwd=2)
+  	lines(f_plot$r,rep(1,length(f_plot$r)),col="black",lty=2,lwd=2)
 	axis(1, at=log10Tck('x','major'), tcl= 0.5,cex.axis=1.5) # bottom
 	axis(1, at=log10Tck('x','minor'), tcl= 0.1, labels=NA) # bottom
 	axis(2,tcl=0.5,cex.axis=1.5) # left
@@ -66,23 +73,65 @@ for (s1 in 1:length(unique_sp)){
 
 	for(s2 in 1:length(unique_sp)){
 		f_plot=subset(f,sp1==unique_sp[s1]&sp2==unique_sp[s2])
-		if(s1==s2){
 		lines(f_plot$r,f_plot$pcf,lty=1,col=colo[s1],lwd=2)
-		}else{
-			dd=c(dd,f_plot$pcf[f_plot$r<0.01])
-		}
 		points(f_plot$r,f_plot$pcf,lty=1,col=colo[s2])
 	}
 }
 
-plot(1,1,t="n",xlim=range(f$r)+0.00001,ylim=range(f$dominance,na.rm=T),xlab="r",ylab="dominance",log="x")
+plot(1,1,t="n",xlim=range(f$r[f$r>0]),ylim=range(f$dominance,na.rm=T),xlab="r",ylab="dominance",log="x",cex.lab=1.5,cex.axis=1.5)
 for (s1 in 1:length(unique_sp)){
 	f_plot=subset(f,sp1==unique_sp[s1]&sp2==unique_sp[s1])
 	points(f_plot$r,f_plot$dominance,col=colo[s1])
 }
 legend("topright",paste("SP=",unique_sp),col=colo,lty=1,bty="n")
 
-#hist(log(dd),main="log(pcf),r<0.01",xlab="log(pcf)")
-#abline(v=0)
+nb_simu=2 #Thomas
+f=read.table(paste("../simulation/pcf_",nb_simu,".txt",sep=""),sep=";",header=F,dec=".")
+colnames(f)=c("r","sp1","sp2","pcf","dominance")
+unique_sp=unique(f$sp1)
+if (length(unique_sp)>length(colo)){
+        colo=rainbow(length(unique_sp))
+}
+
+#Count species
+f_count=read.table(paste("../simulation/nb_indiv_",nb_simu,".txt",sep=""),header=F,sep=";",dec='.')
+colnames(f_count)=c("species","abundance")
+
+#Param simu
+f_param=read.table(paste("../simulation/param_",nb_simu,".txt",sep=""),header=F,sep="=",dec='.')
+f_param[,2]=as.numeric(as.character(f_param[,2]))
+
+C_parent=f_param[f_param[,1]=="N_parent",2]/f_param[f_param[,1]=="volume",2]
+sigma=f_param[f_param[,1]=="sigma",2]
+
+dd=c()
+for (s1 in 1:length(unique_sp)){
+	if(s1==1){
+		yl="g(r)"
+	}else{
+		yl=""
+	}	
+	plot(0.1,0.1,t="n",log="xy",xlab="r",ylab=yl,axes=F,xlim=range(f$r[f$r>0]),cex.lab=1.5,ylim=range(f$pcf))
+        f_plot=subset(f,sp1==unique_sp[s1]&sp2==unique_sp[s1])
+  	th_thomas=1+exp(-f_plot$r^2/(4*sigma^2))*(4*pi*sigma^2)^(-3/2)*1/C_parent
+        lines(f_plot$r,th_thomas,col="black",lty=2,lwd=2)
+        axis(1, at=log10Tck('x','major'), tcl= 0.5,cex.axis=1.5) # bottom
+        axis(1, at=log10Tck('x','minor'), tcl= 0.1, labels=NA) # bottom
+        axis(2,tcl=0.5,cex.axis=1.5) # left
+        box()
+
+        for(s2 in 1:length(unique_sp)){
+                f_plot=subset(f,sp1==unique_sp[s1]&sp2==unique_sp[s2])
+                lines(f_plot$r,f_plot$pcf,lty=1,col=colo[s1],lwd=2)
+                points(f_plot$r,f_plot$pcf,lty=1,col=colo[s2])
+        }
+}
+
+plot(1,1,t="n",xlim=range(f$r[f$r>0]),ylim=range(f$dominance,na.rm=T),xlab="r",ylab="dominance",log="x",cex.axis=1.5,cex.lab=1.5)
+for (s1 in 1:length(unique_sp)){
+        f_plot=subset(f,sp1==unique_sp[s1]&sp2==unique_sp[s1])
+        points(f_plot$r,f_plot$dominance,col=colo[s1])
+}
+legend("topright",paste("SP=",unique_sp),col=colo,lty=1,bty="n")
 
 dev.off()
